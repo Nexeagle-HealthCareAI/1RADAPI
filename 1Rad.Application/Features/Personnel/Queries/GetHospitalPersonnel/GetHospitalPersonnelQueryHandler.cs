7b62e62110f1a610030e5729ea06d1550ec46202
Tasquil Noori <mtnoori40@gmail.com>
2026-05-20 20:54:08 +0530

    Fix: Fetch raw mappings before projecting PersonnelDto to resolve EF Core translation error

--- 1Rad.Application/Features/Personnel/Queries/GetHospitalPersonnel/GetHospitalPersonnelQueryHandler.cs
+++ 1Rad.Application/Features/Personnel/Queries/GetHospitalPersonnel/GetHospitalPersonnelQueryHandler.cs
@@ -15,24 +15,25 @@ public class GetHospitalPersonnelQueryHandler : IRequestHandler<GetHospitalPerso
 
     public async Task<List<PersonnelDto>> Handle(GetHospitalPersonnelQuery request, CancellationToken cancellationToken)
     {
-        return await _context.UserHospitalMappings
+        var mappings = await _context.UserHospitalMappings
             .Where(m => m.HospitalId == request.HospitalId)
             .Include(m => m.User)
             .Include(m => m.Roles)
             .Include(m => m.CustomRoles)
-            .Select(m => new PersonnelDto(
-                m.User.UserId,
-                m.User.FullName,
-                m.User.Email,
-                m.User.Mobile,
-                m.User.Password,
-                m.Roles.Select(r => r.RoleName).Concat(m.CustomRoles.Select(cr => cr.RoleName)).ToList(),
-                m.User.Specialization,
-                m.User.Degree,
-                m.User.LicenseNo,
-                m.User.Status.ToString(),
-                m.User.CreatedAt
-            ))
             .ToListAsync(cancellationToken);
+
+        return mappings.Select(m => new PersonnelDto(
+            m.User.UserId,
+            m.User.FullName,
+            m.User.Email,
+            m.User.Mobile,
+            m.User.Password,
+            m.Roles.Select(r => r.RoleName).Concat(m.CustomRoles.Select(cr => cr.RoleName)).ToList(),
+            m.User.Specialization,
+            m.User.Degree,
+            m.User.LicenseNo,
+            m.User.Status.ToString(),
+            m.User.CreatedAt
+        )).ToList();
     }
 }
