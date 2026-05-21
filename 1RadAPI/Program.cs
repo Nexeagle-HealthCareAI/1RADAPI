d05fb3e0a67e9ff50bbd3ae6e7d77858fc0551c9
Tasquil Noori <mtnoori40@gmail.com>
2026-05-21 19:20:37 +0530

    update

--- 1RadAPI/Program.cs
+++ 1RadAPI/Program.cs
@@ -17,7 +17,6 @@ builder.Services.AddControllers()
     .AddJsonOptions(options => 
     {
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-        options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
     });
 builder.Services.AddFluentValidationAutoValidation()
                 .AddFluentValidationClientsideAdapters();
